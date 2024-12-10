2c31739fb8a657dd8bdfe571ac3a51e76e257602
martin.cong <martin.cong@zhulinkeji.com>
2024-12-10 23:58:51 +0800

    step 3

--- examples/loader/src/main.rs
+++ examples/loader/src/main.rs
@@ -1,5 +1,6 @@
 #![cfg_attr(feature = "axstd", no_std)]
 #![cfg_attr(feature = "axstd", no_main)]
+#![feature(asm_const)]
 
 use core::mem::size_of;
 
@@ -19,6 +20,11 @@ struct PartHeader {
 
 const HEAD_SIZE: usize = size_of::<PartHeader>();
 
+// app running aspace
+// SBI(0x80000000) -> App <- Kernel(0x80200000)
+// va_pa_offset: 0xffff_ffc0_0000_0000
+const RUN_START: usize = 0xffff_ffc0_8010_0000;
+
 #[cfg_attr(feature = "axstd", no_mangle)]
 fn main() {
     // let apps_size = 32; // Dangerous!!! We need to get accurate size of apps.
@@ -36,16 +42,31 @@ fn main() {
         // let part_ptr: *const PartHeader = ;
 
         // println!("field1: {:X} , magic {:X}", part.magic, SALUTE_MAGIC); // Output: 78563412 (little-endian)
-        let apps_size = u32::from_be_bytes(part.length_be) as usize;
+        let app_size = u32::from_be_bytes(part.length_be) as usize;
 
         // println!("field2: {:?} -> len  {}", part.length_be, apps_size);
 
-        let code = unsafe {
-            core::slice::from_raw_parts((apps_start + HEAD_SIZE) as *const u8, apps_size)
-        };
-        apps_start += HEAD_SIZE + apps_size;
+        let code =
+            unsafe { core::slice::from_raw_parts((apps_start + HEAD_SIZE) as *const u8, app_size) };
+        apps_start += HEAD_SIZE + app_size;
         println!("content: {:?}: ", code);
 
+        let run_code = unsafe { core::slice::from_raw_parts_mut(RUN_START as *mut u8, app_size) };
+        run_code.copy_from_slice(code);
+        println!("run code {:?}; address [{:?}]", run_code, run_code.as_ptr());
+
         println!("Load payload {} ok!\n", app_num);
+
+        println!("Execute app {}  ...", app_num);
+
+        // execute app
+        unsafe {
+            core::arch::asm!("
+            li      t2, {run_start}
+            jalr    t2
+            j       .",
+                run_start = const RUN_START,
+            )
+        }
     }
 }
